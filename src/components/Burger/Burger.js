802708d414b901064335746e39e8647c5b3f98ab
noahsung <noah.sung@contentthread.com>
2021-03-11 00:37:41 -0500

    BB-129 burger component that takes a dynamic input

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,21 @@ import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  return (
+
+    // transform Ingredients key-value pair into an array of ingredients
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey}/>;
+        });
+    });
+    return (
       <div className={classes.Burger}>
           <BurgerIngredient type="bread-top"  />
-          <BurgerIngredient type="cheese"  />
-          <BurgerIngredient type="meat"  />
+          {transformedIngredients}
           <BurgerIngredient type="bread-bottom"  />
       </div>
-  );
+    );
 };
 
 export default burger;
