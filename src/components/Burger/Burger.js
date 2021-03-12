c625748a297766ad5b1aa22f18bbaa64f02c98a5
noahsung <noah.sung@contentthread.com>
2021-03-11 22:53:27 -0500

    BB-130 add reduce() to handle empty ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,12 +6,18 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger = (props) => {
 
     // transform Ingredients key-value pair into an array of ingredients
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey}/>;
         });
-    });
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
     return (
       <div className={classes.Burger}>
           <BurgerIngredient type="bread-top"  />
