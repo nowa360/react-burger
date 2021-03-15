c7072ab16d0d0f561957bea5364ebc04c99bec95
nowa360 <noahsung1226@hotmail.com>
2021-03-14 21:38:55 -0400

    BB-146 create sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/ToolBar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <div>Toolbar, Sidedraw, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
