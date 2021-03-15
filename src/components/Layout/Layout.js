968d64057b9e8fbf14bce051ffe263726f376011
nowa360 <noahsung1226@hotmail.com>
2021-03-15 00:21:07 -0400

    BB-150 drawer-toggle and hamburger icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,19 +8,26 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
 
         state = {
-                showSideDrawer: true
+                showSideDrawer: false
         }
         sideDrawerClosedHandler = () => {
                 this.setState({showSideDrawer: false})
         }
+
+        sideDrawerToggleHandler = () => {
+            this.setState((prevState) => {
+                return { showSideDrawer: !prevState.showSideDrawer };
+            });
+        }
+
         render() {
                 return(
                     <Aux>
-                            <Toolbar />
+                            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
                             <SideDrawer
                                 open={this.state.showSideDrawer}
                                 closed={this.sideDrawerClosedHandler} />
-                            <div>Toolbar, Sidedraw, Backdrop</div>
+                            <div>Toolbar, Sidedrawer, Backdrop</div>
                             <main className={classes.Content}>
                                     {this.props.children}
                             </main>
