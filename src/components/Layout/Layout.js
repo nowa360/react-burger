65c18d1669fa7d06d3e442e8ad0921cca0127bcf
nowa360 <noahsung1226@hotmail.com>
2021-03-14 20:47:28 -0400

    BB-144 toolbar and logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/ToolBar';
 
 const layout = (props) => (
     <Aux>
+        <Toolbar />
         <div>Toolbar, Sidedraw, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
