6574b10042d5974b33b88a023a7d55ed74e339fc
nowa360 <noahsung1226@hotmail.com>
2021-03-14 23:36:42 -0400

    BB-148 navigation css

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,20 +2,27 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux'
 
 const sideDrawer = (props) => {
 
     return (
 
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
-            </div>
+        <Aux>
+            <Backdrop show>
 
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+            </Backdrop>
+
+            <div className={classes.SideDrawer}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
     );
 }
 
